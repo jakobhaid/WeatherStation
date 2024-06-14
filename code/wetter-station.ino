d57512efa03f2efa0eee49ceab286ebe04060cc4
Jakob <64287033+jakobhaid@users.noreply.github.com>
2024-06-14 15:20:42 +0200

    Update wetter-station.ino

--- code/wetter-station.ino
+++ code/wetter-station.ino
@@ -6,63 +6,67 @@
 #include <Adafruit_GFX.h>
 #include <BME280I2C.h>
 
-#include <Fonts/FreeMono9pt7b.h>
-#include <Fonts/FreeMono12pt7b.h>
 #include <Fonts/FreeMonoBold12pt7b.h>
+// #include <Fonts/FreeMono12pt7b.h>
+// #include <Fonts/FreeMono9pt7b.h>
 
 //BME688
-#define BME_SDA D4
-#define BME_SCL D5
 Adafruit_BME680 bme68;
 
 //BME280
-BME280I2C bme28;
+BME280I2C::Settings settings(
+  BME280::OSR_X2, BME280::OSR_X16, 
+  BME280::OSR_X8, BME280::Mode_Normal, 
+  BME280::StandbyTime_125ms, BME280::Filter_16, 
+  BME280::SpiEnable_False, BME280I2C::I2CAddr_0x76);
+BME280I2C bme28(settings);
 
 //E-Paper-Display
 #define EPAPER_CS D1
 #define EPAPER_DC D3
 #define EPAPER_RST D0
-#define EPAPER_BUSY D2 //D5
+#define EPAPER_BUSY D2
 GxEPD2_BW<GxEPD2_290_BS, GxEPD2_290_BS::HEIGHT> display(GxEPD2_290_BS(/*CS=5*/ EPAPER_CS, /*DC=*/ EPAPER_DC, /*RES=*/ EPAPER_RST, /*BUSY=*/ EPAPER_BUSY)); // DEPG0290BS 128x296, SSD1680
 
 // Globale variablen
-float temp(NAN),     hum(NAN),     pres(NAN),     gas(NAN),     alt(NAN);
-// float lastTemp(NAN), lastHum(NAN), lastPres(NAN), lastGas(NAN), lastAlt(NAN);
-float temp68(NAN),   hum68(NAN),   pres68(NAN),   gas68(NAN),   alt68(NAN);
-float temp28(NAN),   hum28(NAN),   pres28(NAN);
+RTC_DATA_ATTR float temp,   hum,   pres,   gas,   alt;
+RTC_DATA_ATTR float lastTemp, lastHum, lastPres, lastGas, lastAlt;
+float temp68, hum68, pres68, gas68, alt68;
+float temp28, hum28, pres28;
 
 // Makro für Serial-Ausgaben
-#define SERIAL_DEBUG_BEGIN()   do { if (Serial) { Serial.begin(9600); while (!Serial); } } while (0)
 #define SERIAL_PRINT(...)      do { if (Serial) { Serial.print(__VA_ARGS__); } } while (0)
 #define SERIAL_PRINTLN(...)    do { if (Serial) { Serial.println(__VA_ARGS__); } } while (0)
 
 // Deep Sleep
 #define uS_TO_S_FACTOR 1000000ULL  /* Conversion factor for micro seconds to seconds */
-#define TIME_TO_SLEEP  600        /* Time ESP32 will go to sleep (in seconds) */
+#define TIME_TO_SLEEP  60 * 15     /* Time ESP32 will go to sleep (in seconds) */
+RTC_DATA_ATTR int bootCount = 0;
+#define BUTTON_PIN_BITMASK 0x100 // 2^8 in hex
 
 void setup() {
-  delay(1000);
-  SERIAL_DEBUG_BEGIN();
+  try {Serial.begin(9600);
+  }catch(...){}
 
   Wire.begin();
 
-  // BME688 initialisieren
-  while (!bme68.begin()) {  // Standard I2C Adresse 0x76
+  // Initialize BME688
+  while (!bme68.begin()) { // Standard I2C Adresse 0x76
     SERIAL_PRINTLN("Could not find BME688 sensor!");
-    delay(1000);
+    delay(5000);
   }
 
-  // BME280 initialisieren
-  while (!bme28.begin()){
+  // Initialize BME280
+  while (!bme28.begin()){ // Standard I2C Adresse 0x77
     SERIAL_PRINTLN("Could not find BME280 sensor!");
-    delay(1000);
+    delay(5000);
   }
 
   // Setze die Sensorparameter für den BME688
-  bme68.setTemperatureOversampling(BME680_OS_8X);
-  bme68.setHumidityOversampling(BME680_OS_2X);
-  bme68.setPressureOversampling(BME680_OS_4X);
-  bme68.setIIRFilterSize(BME680_FILTER_SIZE_3);
+  bme68.setTemperatureOversampling(BME680_OS_2X);
+  bme68.setHumidityOversampling(BME680_OS_1X);
+  bme68.setPressureOversampling(BME680_OS_16X);
+  bme68.setIIRFilterSize(BME680_FILTER_SIZE_127);
   bme68.setGasHeater(320, 150); // 320°C für 150 ms
 
   // E-Paper-Display initialisieren
@@ -77,17 +81,26 @@ void loop() {
   readSensor280();
   calculateValues();
   printValues();
-  displayData();
 
-  // delay(600000);
+  if (bootCount % 10 == 0){
+    displayValuesFull();
+    ++bootCount;
+  }else {
+    displayValuesPartiell();
+    ++bootCount;
+  }
+
+  // delay(10000);
+  // esp_sleep_enable_ext0_wakeup(GPIO_NUM_33, 0); //1 = High, 0 = Low
   esp_sleep_enable_timer_wakeup(TIME_TO_SLEEP * uS_TO_S_FACTOR);
   esp_deep_sleep_start();
 }
 
-void readSensor688(){
+void readSensor688 (){
   if (! bme68.performReading()) {
     SERIAL_PRINTLN("Failed to perform reading on BME688");
     delay(200);
+    temp68 = hum68 = pres68 = gas68 = alt68 = NAN;
     return;
   } else {
     temp68 = bme68.temperature;
@@ -98,7 +111,7 @@ void readSensor688(){
   }
 }
 
-void readSensor280(){
+void readSensor280 (){
   BME280::TempUnit tempUnit(BME280::TempUnit_Celsius);
   BME280::PresUnit presUnit(BME280::PresUnit_hPa);
 
@@ -106,10 +119,11 @@ void readSensor280(){
     bme28.read(pres28, temp28, hum28, tempUnit, presUnit);
   } catch(...) {
     SERIAL_PRINTLN("Failed to perform reading on BME280");
+    temp28 = hum28 = pres28 = NAN;
   }
 }
 
-void printValues() {
+void printValues () {
   //print BME688
   SERIAL_PRINT("Sensor BME688: Temp = ");
   SERIAL_PRINT(temp68);
@@ -147,7 +161,14 @@ void printValues() {
   SERIAL_PRINTLN();
 }
 
-void calculateValues(){
+void calculateValues (){
+  // Store previous values
+  lastTemp = temp;
+  lastHum = hum;
+  lastPres = pres;
+  lastGas = gas;
+  lastAlt = alt;
+
   // Durchschnittstemperatur berechnen
   if (!isnan(temp68) && !isnan(temp28)) {
     temp = (temp68 + temp28) / 2.0;
@@ -159,7 +180,7 @@ void calculateValues(){
     temp = NAN;
   }
 
-  // Durchschnittsfeuchtigkeit berechnen
+  /* Durchschnittsfeuchtigkeit berechnen   deaktiviert
   if (!isnan(hum68) && !isnan(hum28)) {
     hum = (hum68 + hum28) / 2.0;
   } else if (!isnan(hum68)) {
@@ -168,7 +189,8 @@ void calculateValues(){
     hum = hum28;
   } else {
     hum = NAN;
-  }
+  }*/
+  hum = hum68;
 
   // Durchschnittsdruck berechnen
   if (!isnan(pres68) && !isnan(pres28)) {
@@ -186,28 +208,79 @@ void calculateValues(){
   alt = alt68;
 }
 
-void displayData(){
+void displayValuesPartiell (){
+  // Calculate the update areas
+  display.fillScreen(GxEPD_WHITE);
+  int16_t x1, y1;
+  uint16_t w, h;
+
+  String tempStr = "Temp:  " + String(temp) + "C    " + getTrendSymbol(temp, lastTemp);
+  String presStr = "";
+  if(pres < 1000.0){
+    presStr = "Pres:  " + String(pres) + "hPa " + getTrendSymbol(pres, lastPres);
+  }else{
+    presStr = "Pres: " + String(pres) + "hPa " + getTrendSymbol(pres, lastPres);
+  }
+  String humStr = "Hum:   " + String(hum) + "%    " + getTrendSymbol(hum, lastHum);
+  String gasStr = "Gas:   " + String(gas) + "KOhms" + getTrendSymbol(gas, lastGas);
+  String altStr = "Alt:   " + String(alt) + "m   " + getTrendSymbol(alt, lastAlt);
+
+  display.getTextBounds(tempStr, 35, 20, &x1, &y1, &w, &h);
+  display.fillRect(x1, y1, w, h, GxEPD_WHITE);
+  display.setCursor(35, 20);
+  display.print(tempStr);
+
+  display.getTextBounds(presStr, 35, 40, &x1, &y1, &w, &h);
+  display.fillRect(x1, y1, w, h, GxEPD_WHITE);
+  display.setCursor(35, 40);
+  display.print(presStr);
+
+  display.getTextBounds(humStr, 35, 60, &x1, &y1, &w, &h);
+  display.fillRect(x1, y1, w, h, GxEPD_WHITE);
+  display.setCursor(35, 60);
+  display.print(humStr);
+
+  display.getTextBounds(gasStr, 35, 80, &x1, &y1, &w, &h);
+  display.fillRect(x1, y1, w, h, GxEPD_WHITE);
+  display.setCursor(35, 80);
+  display.print(gasStr);
+
+  display.getTextBounds(altStr, 35, 100, &x1, &y1, &w, &h);
+  display.fillRect(x1, y1, w, h, GxEPD_WHITE);
+  display.setCursor(35, 100);
+  display.print(altStr);
+
+  // Update display with partial update
+  display.display(true); // partial update
+}
+
+void displayValuesFull (){
   //display.setFullWindow();
   display.fillScreen(GxEPD_WHITE);
 
   display.setCursor(35, 20);
-  display.print("Temp:  " + (String)temp + "C");
+  display.print("Temp:  " + String(temp) + "C    " + getTrendSymbol(temp, lastTemp));
 
   display.setCursor(35, 40);
-  if((bme68.pressure / 100.0) < 1000){
-    display.print("Press:  " + (String)pres + "hPa");
+  if(pres < 1000.0){
+    display.print("Pres:  " + String(pres) + "hPa " + getTrendSymbol(pres, lastPres));
   }else{
-    display.print("Press: " + (String)pres + "hPa");
+    display.print("Pres: " + String(pres) + "hPa " + getTrendSymbol(pres, lastPres));
   }
 
   display.setCursor(35, 60);
-  display.print("Hum:   " + (String)hum + "%");
+  display.print("Hum:   " + String(hum) + "%    " + getTrendSymbol(hum, lastHum));
 
   display.setCursor(35, 80);
-  display.print("Gas:   " + (String)gas + "KOhms");
+  display.print("Gas:   " + String(gas) + "KOhms" + getTrendSymbol(gas, lastGas));
 
   display.setCursor(35, 100);
-  display.print("Alt:   " + (String)alt + "m");
+  display.print("Alt:   " + String(alt) + "m   " + getTrendSymbol(alt, lastAlt));
 
   display.display();
+}
+
+String getTrendSymbol (float current, float previous) {
+  if (isnan(previous)) return "";  // No previous value
+  return (current > previous) ? "+" : ((current < previous) ? "-" : "=");
 }
